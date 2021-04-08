aa9b2d0fc1c25325a001df462727e92e8ba77f2e
xrubenruizx <61049993+xrubenruizx@users.noreply.github.com>
2021-04-08 11:08:48 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	char opcion;
+	int opcion;
 	printf("Selecciona juego: \n 1.Pong\n 2.Ahorcado\n 3.Tres en ralla\n 4.Buscaminas\n 5.Hundir la flota\n 6.Snake\n 7.Salir\n");
 	do
 	{
-		scanf("%c",&opcion);
+		scanf("%i",&opcion);
 		switch(opcion)
 		{
 			case '1':
